f96f33f3bd90e116705dac5b87af89c30263e80a
miner409 <fraser.crawford1@gmail.com>
2024-12-10 13:59:42 +0000

    Added pixels code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use pixels::{Pixels, SurfaceTexture};
 use winit::application::ApplicationHandler;
 use winit::error::EventLoopError;
 use winit::event::WindowEvent;
@@ -8,25 +9,27 @@ use winit_input_helper::WinitInputHelper;
 struct App {
     window: Option<Window>,
     title: String,
+    width: u32,
+    height: u32,
+    pixels: Option<Pixels>
 }
 impl App {
-    fn new<T:Into<String>>(title: T) -> Self {
-        App{window: None, title: title.into()}
-    }
-}
-impl Default for App {
-    fn default() -> Self {
-        App {
-            window: None,
-            title: String::from("Title"),
-        }
+    fn new<T:Into<String>>(title: T,width:u32,height:u32) -> Self {
+        App{window: None,pixels:None, title: title.into(),width,height}
     }
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         let attributes = Window::default_attributes().with_title(&self.title);
-        self.window = Some(event_loop.create_window(attributes).unwrap());
+        let window = event_loop.create_window(attributes).unwrap();
+        let pixels = {
+            let window_size = window.inner_size();
+            let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+            Pixels::new(self.width, self.height, surface_texture).expect("Pixels::new failed")
+        };
+        self.pixels = Some(pixels);
+        self.window = Some(window);
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
@@ -35,7 +38,22 @@ impl ApplicationHandler for App {
                 println!("The close button was pressed; stopping");
                 event_loop.exit();
             }
+            WindowEvent::Resized(size) => {
+                match self.pixels.as_mut() {
+                    None => {}
+                    Some(pixels) => {pixels.resize_surface(size.width, size.height).expect("resize failed")}
+                }
+            }
+            WindowEvent::KeyboardInput {event, .. }=> {
+                if event.state == winit::event::ElementState::Pressed {
+                    println!("{:?}", event.text);
+                }
+            }
             WindowEvent::RedrawRequested => {
+                match self.pixels.as_mut() {
+                    None => {}
+                    Some(pixels) => {pixels.render().expect("pixels render failed")}
+                }
                 self.window.as_ref().unwrap().request_redraw();
             }
             _ => (),
@@ -43,15 +61,13 @@ impl ApplicationHandler for App {
     }
 }
 
-pub fn make_main<T:Into<String>>(polling: bool, window_title: T) -> Result<(), EventLoopError> {
+pub fn make_main<T:Into<String>>(polling: bool, window_title: T, width:u32, height: u32) -> Result<(), EventLoopError> {
     let event_loop = EventLoop::new()?;
-    let mut input = WinitInputHelper::new();
-
     if polling {
         event_loop.set_control_flow(ControlFlow::Poll);
     } else {
         event_loop.set_control_flow(ControlFlow::Wait);
     }
-    let mut app = App::new(window_title);
+    let mut app = App::new(window_title,width,height);
     event_loop.run_app(&mut app)
 }
